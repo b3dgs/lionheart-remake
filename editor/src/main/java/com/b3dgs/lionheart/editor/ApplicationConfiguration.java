85e341a5cdfa28e8c4f36ddc9f28a366920ff449
DjThunder <contact@b3dgs.com>
2014-09-13 21:55:38 +0200

    #5: Splash screen added, application title adapted, import project at startup.

--- editor/src/main/java/com/b3dgs/lionheart/editor/ApplicationConfiguration.java
+++ editor/src/main/java/com/b3dgs/lionheart/editor/ApplicationConfiguration.java
@@ -17,11 +17,22 @@
  */
 package com.b3dgs.lionheart.editor;
 
+import java.io.File;
+
+import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
-import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.e4.core.services.events.IEventBroker;
 import org.eclipse.e4.ui.model.application.MApplication;
 import org.eclipse.e4.ui.model.application.ui.basic.MWindow;
+import org.eclipse.e4.ui.workbench.UIEvents;
+import org.eclipse.e4.ui.workbench.modeling.EPartService;
+import org.osgi.service.event.Event;
+import org.osgi.service.event.EventHandler;
+
+import com.b3dgs.lionengine.editor.project.ImportProjectHandler;
+import com.b3dgs.lionengine.editor.project.Project;
 
 /**
  * Configure the editor with the right name.
@@ -33,14 +44,58 @@ public class ApplicationConfiguration
     /** Application reference. */
     @Inject
     MApplication application;
+    /** Part service reference. */
+    @Inject
+    EPartService partService;
 
     /**
      * Execute the injection.
+     * 
+     * @param eventBroker The event broker service.
      */
-    @Execute
-    public void execute()
+    @PostConstruct
+    public void execute(IEventBroker eventBroker)
     {
         final MWindow existingWindow = application.getChildren().get(0);
         existingWindow.setLabel("Lionheart Remake Editor");
+
+        eventBroker.subscribe(UIEvents.UILifeCycle.APP_STARTUP_COMPLETE, new AppStartupCompleteEventHandler());
+    }
+
+    /**
+     * Handler called on startup complete.
+     */
+    private class AppStartupCompleteEventHandler
+            implements EventHandler
+    {
+        /**
+         * Constructor.
+         */
+        public AppStartupCompleteEventHandler()
+        {
+            // Nothing to do
+        }
+
+        @Override
+        public void handleEvent(Event event)
+        {
+            final String[] args = Platform.getApplicationArgs();
+            for (int i = 0; i < args.length; i++)
+            {
+                if (args[i].equals("-import"))
+                {
+                    i++;
+                    if (i < args.length)
+                    {
+                        final File path = new File(args[i]);
+                        if (path.isDirectory())
+                        {
+                            final Project project = Project.create(path);
+                            ImportProjectHandler.importProject(project, partService);
+                        }
+                    }
+                }
+            }
+        }
     }
 }
