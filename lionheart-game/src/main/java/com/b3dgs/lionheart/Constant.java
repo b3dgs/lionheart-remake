97e467f27aefd09b6cc4c64151f22f0f803c649f
DjThunder <contact@b3dgs.com>
2018-11-04 10:28:13 +0100

    Version updated to 0.0.9.

--- lionheart-game/src/main/java/com/b3dgs/lionheart/Constant.java
+++ lionheart-game/src/main/java/com/b3dgs/lionheart/Constant.java
@@ -29,7 +29,7 @@ public final class Constant
     /** Application name. */
     public static final String PROGRAM_NAME = "Lionheart Remake";
     /** Application version. */
-    public static final Version PROGRAM_VERSION = Version.create(0, 0, 8);
+    public static final Version PROGRAM_VERSION = Version.create(0, 0, 9);
 
     /** Original display. */
     public static final Resolution NATIVE_RESOLUTION = new Resolution(280, 220, 60);
