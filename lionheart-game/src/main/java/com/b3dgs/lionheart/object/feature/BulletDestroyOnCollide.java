a1e4b60469d02a9740ad06d691eee26a9fc8e500
DjThunder <contact@b3dgs.com>
2021-02-23 14:19:31 +0100

    #187: Dragonfly Objects Dragon fireball destroy on hit and screen exit.

---
+++ lionheart-game/src/main/java/com/b3dgs/lionheart/object/feature/BulletDestroyOnCollide.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) 2013-2021 Byron 3D Games Studio (www.b3dgs.com) Pierre-Alexandre (contact@b3dgs.com)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.b3dgs.lionheart.object.feature;
+
+import com.b3dgs.lionengine.LionEngineException;
+import com.b3dgs.lionengine.Viewer;
+import com.b3dgs.lionengine.game.feature.FeatureGet;
+import com.b3dgs.lionengine.game.feature.FeatureInterface;
+import com.b3dgs.lionengine.game.feature.FeatureModel;
+import com.b3dgs.lionengine.game.feature.Identifiable;
+import com.b3dgs.lionengine.game.feature.Routine;
+import com.b3dgs.lionengine.game.feature.Services;
+import com.b3dgs.lionengine.game.feature.Setup;
+import com.b3dgs.lionengine.game.feature.Transformable;
+import com.b3dgs.lionengine.game.feature.collidable.Collidable;
+import com.b3dgs.lionengine.game.feature.collidable.CollidableListener;
+import com.b3dgs.lionengine.game.feature.collidable.Collision;
+
+/**
+ * Destroy bullet on collide.
+ */
+@FeatureInterface
+public final class BulletDestroyOnCollide extends FeatureModel implements Routine, CollidableListener
+{
+    private final Viewer viewer = services.get(Viewer.class);
+
+    @FeatureGet private Transformable transformable;
+    @FeatureGet private Identifiable identifiable;
+
+    /**
+     * Create feature.
+     * 
+     * @param services The services reference (must not be <code>null</code>).
+     * @param setup The setup reference (must not be <code>null</code>).
+     * @throws LionEngineException If invalid arguments.
+     */
+    public BulletDestroyOnCollide(Services services, Setup setup)
+    {
+        super(services, setup);
+    }
+
+    @Override
+    public void update(double extrp)
+    {
+        if (!viewer.isViewable(transformable, 0, 0))
+        {
+            identifiable.destroy();
+        }
+    }
+
+    @Override
+    public void notifyCollided(Collidable collidable, Collision with, Collision by)
+    {
+        identifiable.destroy();
+    }
+}
