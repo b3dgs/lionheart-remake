6636db027d6ca7f4c746570071e52f293bb3a50c
Pierre-Alexandre <contact@b3dgs.com>
2021-03-12 23:52:37 +0100

    #14: Extro part5 almost finished.

--- lionheart-game/src/main/java/com/b3dgs/lionheart/extro/Part5.java
+++ lionheart-game/src/main/java/com/b3dgs/lionheart/extro/Part5.java
@@ -16,12 +16,30 @@
  */
 package com.b3dgs.lionheart.extro;
 
+import com.b3dgs.lionengine.AnimState;
+import com.b3dgs.lionengine.Animation;
+import com.b3dgs.lionengine.Media;
 import com.b3dgs.lionengine.Medias;
 import com.b3dgs.lionengine.Origin;
 import com.b3dgs.lionengine.UtilMath;
+import com.b3dgs.lionengine.game.feature.Camera;
+import com.b3dgs.lionengine.game.feature.CameraTracker;
+import com.b3dgs.lionengine.game.feature.ComponentDisplayable;
+import com.b3dgs.lionengine.game.feature.ComponentRefreshable;
+import com.b3dgs.lionengine.game.feature.Factory;
+import com.b3dgs.lionengine.game.feature.Featurable;
+import com.b3dgs.lionengine.game.feature.Handler;
+import com.b3dgs.lionengine.game.feature.Services;
+import com.b3dgs.lionengine.game.feature.Spawner;
+import com.b3dgs.lionengine.game.feature.Transformable;
 import com.b3dgs.lionengine.graphic.Graphic;
 import com.b3dgs.lionengine.graphic.drawable.Drawable;
+import com.b3dgs.lionengine.graphic.drawable.Sprite;
 import com.b3dgs.lionengine.graphic.drawable.SpriteAnimated;
+import com.b3dgs.lionengine.graphic.engine.SourceResolutionProvider;
+import com.b3dgs.lionengine.helper.MapTileHelper;
+import com.b3dgs.lionheart.CheckpointHandler;
+import com.b3dgs.lionheart.Constant;
 import com.b3dgs.lionheart.constant.Folder;
 import com.b3dgs.lionheart.intro.Intro;
 
@@ -30,27 +48,49 @@ import com.b3dgs.lionheart.intro.Intro;
  */
 public final class Part5
 {
-    private final SpriteAnimated transform0 = Drawable.loadSpriteAnimated(Medias.create(Folder.EXTRO,
-                                                                                        "part5",
-                                                                                        "transform0.png"),
-                                                                          2,
-                                                                          1);
-    private final SpriteAnimated transform1 = Drawable.loadSpriteAnimated(Medias.create(Folder.EXTRO,
-                                                                                        "part5",
-                                                                                        "transform1.png"),
-                                                                          4,
-                                                                          3);
-    private final SpriteAnimated transform2 = Drawable.loadSpriteAnimated(Medias.create(Folder.EXTRO,
-                                                                                        "part5",
-                                                                                        "transform2.png"),
-                                                                          10,
-                                                                          2);
-    private final SpriteAnimated transform3 = Drawable.loadSpriteAnimated(Medias.create(Folder.EXTRO,
-                                                                                        "part5",
-                                                                                        "transform3.png"),
-                                                                          4,
-                                                                          2);
-    private double alphaBack;
+    private static final Animation OPEN = new Animation("open", 1, 8, 0.18, false, false);
+
+    private final Sprite transform0a = get("transform0a.png");
+    private final Sprite transform0b = get("transform0b.png");
+    private final Sprite transform0c = get("transform0c.png");
+    private final SpriteAnimated transform1 = get("transform1.png", 5, 2);
+    private final SpriteAnimated transform2 = get("transform2.png", 4, 3);
+    private final SpriteAnimated transform3 = get("transform3.png", 10, 2);
+    private final SpriteAnimated transform4 = get("transform4.png", 4, 2);
+    private final SpriteAnimated eyes = get("eyes.png", 2, 4);
+
+    private static Sprite get(String file)
+    {
+        return Drawable.loadSprite(Medias.create(Folder.EXTRO, "part5", file));
+    }
+
+    private static SpriteAnimated get(String file, int horizontalFrames, int verticalFrames)
+    {
+        return Drawable.loadSpriteAnimated(Medias.create(Folder.EXTRO, "part5", file),
+                                           horizontalFrames,
+                                           verticalFrames);
+    }
+
+    private final Services services = new Services();
+    private final Factory factory = services.create(Factory.class);
+    private final Handler handler = services.create(Handler.class);
+    private final Spawner spawner = services.add(new Spawner()
+    {
+        @Override
+        public Featurable spawn(Media media, double x, double y)
+        {
+            final Featurable featurable = factory.create(media);
+            featurable.getFeature(Transformable.class).teleport(x, y);
+            handler.add(featurable);
+            return featurable;
+        }
+    });
+
+    private int alphaBack;
+    private int alpha0b;
+    private int flick0c;
+    private int flicked0c;
+    private boolean spawned;
 
     /**
      * Constructor.
@@ -58,6 +98,37 @@ public final class Part5
     public Part5()
     {
         super();
+
+        final SourceResolutionProvider source = services.add(new SourceResolutionProvider()
+        {
+            @Override
+            public int getWidth()
+            {
+                return Constant.NATIVE_RESOLUTION.getWidth();
+            }
+
+            @Override
+            public int getHeight()
+            {
+                return Constant.NATIVE_RESOLUTION.getHeight();
+            }
+
+            @Override
+            public int getRate()
+            {
+                return Constant.NATIVE_RESOLUTION.getRate();
+            }
+        });
+        final Camera camera = services.create(Camera.class);
+        camera.setView(0, 0, source.getWidth(), source.getHeight(), source.getHeight());
+
+        services.add(new CameraTracker(services));
+        services.add(new MapTileHelper(services));
+        services.add(new CheckpointHandler(services));
+
+        handler.addComponent(new ComponentRefreshable());
+        handler.addComponent(new ComponentDisplayable());
+        handler.addListener(factory);
     }
 
     /**
@@ -65,9 +136,18 @@ public final class Part5
      */
     public void load()
     {
-        transform0.load();
-        transform0.prepare();
-        transform0.setOrigin(Origin.CENTER_TOP);
+        transform0a.load();
+        transform0a.prepare();
+        transform0a.setOrigin(Origin.CENTER_TOP);
+
+        transform0b.load();
+        transform0b.prepare();
+        transform0b.setOrigin(Origin.CENTER_TOP);
+        transform0b.setAlpha(alpha0b);
+
+        transform0c.load();
+        transform0c.prepare();
+        transform0c.setOrigin(Origin.CENTER_TOP);
 
         transform1.load();
         transform1.prepare();
@@ -77,6 +157,10 @@ public final class Part5
 
         transform3.load();
         transform3.prepare();
+
+        eyes.load();
+        eyes.prepare();
+        eyes.setOrigin(Origin.CENTER_TOP);
     }
 
     /**
@@ -87,15 +171,73 @@ public final class Part5
      */
     public void update(long seek, double extrp)
     {
+        handler.update(extrp);
+
         if (seek > 135000 && seek < 138000)
         {
-            alphaBack += 3.0;
+            alphaBack += 3;
         }
         else if (seek > 161000)
         {
-            alphaBack -= 3.0;
+            alphaBack -= 3;
         }
-        alphaBack = UtilMath.clamp(alphaBack, 0.0, 255.0);
+        alphaBack = UtilMath.clamp(alphaBack, 0, 255);
+
+        if (seek > 139600 && seek < 139700 && !spawned)
+        {
+            spawner.spawn(Medias.create(Folder.EXTRO, "part5", "Transform1.xml"), 210, 64);
+            spawned = !spawned;
+        }
+
+        if (seek > 140000 && seek < 145000)
+        {
+            alpha0b += 3;
+            if (alpha0b < 256)
+            {
+                transform0b.setAlpha(UtilMath.clamp(alpha0b, 0, 255));
+            }
+        }
+
+        if (seek > 146000 && seek < 146100 && spawned)
+        {
+            spawner.spawn(Medias.create(Folder.EXTRO, "part5", "Transform2.xml"), 216, 88);
+            spawned = !spawned;
+        }
+        if (seek > 146500 && seek < 146600 && !spawned)
+        {
+            spawner.spawn(Medias.create(Folder.EXTRO, "part5", "Transform3.xml"), 210, 160);
+            spawned = !spawned;
+        }
+        if (seek > 147200 && seek < 147300 && spawned)
+        {
+            spawner.spawn(Medias.create(Folder.EXTRO, "part5", "Transform4.xml"), 210, 160);
+            spawned = !spawned;
+        }
+
+        if (seek > 150000 && flicked0c < 6)
+        {
+            flick0c++;
+            if (flick0c > 16)
+            {
+                flick0c -= 16;
+                flicked0c++;
+            }
+            alpha0b = 255;
+        }
+        if (seek > 154000 && seek < 155000)
+        {
+            alpha0b -= 3;
+            if (alpha0b > -1)
+            {
+                transform0b.setAlpha(UtilMath.clamp(alpha0b, 0, 255));
+            }
+        }
+        if (seek > 155000 && eyes.getAnimState() == AnimState.STOPPED)
+        {
+            eyes.play(OPEN);
+        }
+
+        eyes.update(extrp);
     }
 
     /**
@@ -110,13 +252,27 @@ public final class Part5
     {
         g.clear(0, 0, width, height);
 
-        transform0.setLocation(width / 2, 32);
-        transform0.render(g);
+        transform0a.setLocation(width / 2, 24);
+        transform0a.render(g);
+
+        if (flick0c > 8 || flicked0c > 5)
+        {
+            transform0c.setLocation(width / 2, 24);
+            transform0c.render(g);
+
+            eyes.setLocation(width / 2, 57);
+            eyes.render(g);
+        }
+
+        transform0b.setLocation(width / 2, 24);
+        transform0b.render(g);
+
+        handler.render(g);
 
         // Render fade in
         if (alphaBack < 255)
         {
-            g.setColor(Intro.ALPHAS_BLACK[255 - (int) alphaBack]);
+            g.setColor(Intro.ALPHAS_BLACK[255 - alphaBack]);
             g.drawRect(0, 0, width, height, true);
         }
     }
