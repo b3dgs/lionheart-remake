1a42f19c48520379f8bd96bec3cda19074e89a98
Pierre-Alexandre <contact@b3dgs.com>
2021-03-25 20:09:09 +0100

    #208: Player map limit fixed.

---
+++ lionheart-game/src/main/java/com/b3dgs/lionheart/object/feature/MapLimit.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) 2013-2021 Byron 3D Games Studio (www.b3dgs.com) Pierre-Alexandre (contact@b3dgs.com)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.b3dgs.lionheart.object.feature;
+
+import com.b3dgs.lionengine.LionEngineException;
+import com.b3dgs.lionengine.game.feature.FeatureGet;
+import com.b3dgs.lionengine.game.feature.FeatureInterface;
+import com.b3dgs.lionengine.game.feature.FeatureModel;
+import com.b3dgs.lionengine.game.feature.Routine;
+import com.b3dgs.lionengine.game.feature.Services;
+import com.b3dgs.lionengine.game.feature.Transformable;
+import com.b3dgs.lionengine.game.feature.rasterable.SetupSurfaceRastered;
+import com.b3dgs.lionengine.game.feature.tile.map.MapTile;
+import com.b3dgs.lionheart.object.EntityModel;
+
+/**
+ * Map limit feature implementation.
+ * <ol>
+ * <li>Clamp location on limit.
+ * </ol>
+ */
+@FeatureInterface
+public final class MapLimit extends FeatureModel implements Routine
+{
+    private final MapTile map = services.get(MapTile.class);
+
+    @FeatureGet private Transformable transformable;
+    @FeatureGet private EntityModel model;
+
+    /**
+     * Create feature.
+     * 
+     * @param services The services reference (must not be <code>null</code>).
+     * @param setup The setup reference (must not be <code>null</code>).
+     * @throws LionEngineException If invalid arguments.
+     */
+    public MapLimit(Services services, SetupSurfaceRastered setup)
+    {
+        super(services, setup);
+    }
+
+    @Override
+    public void update(double extrp)
+    {
+        if (transformable.getX() < 2)
+        {
+            transformable.teleportX(2);
+            model.getMovement().zero();
+        }
+        else if (transformable.getX() > map.getWidth() - 16)
+        {
+            transformable.teleportX(map.getWidth() - 16);
+            model.getMovement().zero();
+        }
+    }
+}
