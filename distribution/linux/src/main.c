890a8f84bcb601c029f439b09059ac0e54195bcd
Pierre-Alexandre <contact@b3dgs.com>
2023-04-01 23:29:57 +0200

    [#315] Build resource updated.

---
+++ distribution/linux/src/main.c
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) 2013-2023 Byron 3D Games Studio (www.b3dgs.com) Pierre-Alexandre (contact@b3dgs.com)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+#include <stdlib.h>
+
+int main(int argc, char** argv)
+{
+   system("cd data; jre_linux-x86_64/bin/java -server -splash:splash.png -jar lionheart-pc-1.3.0.jar");
+   return 0;
+}
