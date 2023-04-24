1e8c7683e80281c9921c6dad5155b1eb2aa153f1
Pierre-Alexandre <contact@b3dgs.com>
2023-04-24 15:56:15 +0200

    Update lionheart-remake.sh

--- distribution/linux/sh/lionheart-remake.sh
+++ distribution/linux/sh/lionheart-remake.sh
@@ -3,8 +3,8 @@
 export VERSION=%%INPUT_APPV%%
 export PARAM="-server -splash:splash.png -jar lionheart-pc-"$VERSION".jar"
 
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-cd "$parent_path"
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd "${DIR}"
 
 cd data
 jre_linux-x86_64/bin/java $PARAM
