adb5b3c3a1caa8055d012a3956f6579a1fd4cbfb
Pierre-Alexandre <contact@b3dgs.com>
2023-04-28 10:10:03 +0200

    Update configure.sh

--- distribution/linux/sh/configure.sh
+++ distribution/linux/sh/configure.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-export VERSION=%%INPUT_APPV%%
-export PARAM="-server -splash:splash.png -cp lionheart-pc-"$VERSION".jar com.b3dgs.lionheart.Launcher"
+VERSION=%%INPUT_APPV%%
+PARAM="-server -splash:splash.png -cp lionheart-pc-"$VERSION".jar com.b3dgs.lionheart.Launcher"
 
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-cd "$parent_path"
-
-cd data
-jre_linux-x86_64/bin/java $PARAM
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/data; jre_linux-x86_64/bin/java ${PARAM}
