3ef21f3546eb82d6cafbaeeabaf6ba1a6066124d
Pierre-Alexandre <contact@b3dgs.com>
2023-05-08 16:24:26 +0200

    Update profile.sh

--- distribution/linux/sh/profile.sh
+++ distribution/linux/sh/profile.sh
@@ -24,5 +24,5 @@ ARGS="-game story -stage original"
 VERSION=%%INPUT_APPV%%
 PARAM="-server -splash:splash.png -jar lionheart-pc-"$VERSION".jar "
 
-cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/data; jre_linux-x86_64/bin/java ${PARAM} ${$ARGS}
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/data; jre_linux-x86_64/bin/java ${PARAM} ${ARGS}
 # -------------------------------------------------------------
